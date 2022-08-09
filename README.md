106bc4494a101a3ad6922f329a6396594d72b866
Omer Oreg <sarhatabaot1@gmail.com>
2022-08-09 17:22:54 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CM-IA-Addon
+ ItemsAdder addon for ConfigurableMenus
---
+++ src/main/java/com/lapzupi/dev/itemsadderaddon/itemsadderaddon/ItemsAdderAddon.java
@@ -0,0 +1,71 @@
+package com.lapzupi.dev.itemsadderaddon.itemsadderaddon;
+
+import com.lapzupi.dev.configurablemenus.ConfigurableMenusPlugin;
+import com.lapzupi.dev.configurablemenus.addons.ItemAddon;
+import dev.lone.itemsadder.api.CustomStack;
+import dev.lone.itemsadder.api.Events.ItemsAdderLoadDataEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+public final class ItemsAdderAddon extends ItemAddon implements Listener {
+    private boolean iaStatus = false;
+    private final ConfigurableMenusPlugin plugin;
+
+    public ItemsAdderAddon() {
+        this.plugin = (ConfigurableMenusPlugin) Bukkit.getPluginManager().getPlugin("ConfigurableMenus");
+    }
+
+    @Contract(pure = true)
+    @Override
+    public @NotNull ItemStack getItemStack(final String id) {
+        try {
+            return CustomStack.getInstance(id).getItemStack();
+        } catch (NullPointerException e) {
+            this.plugin.debug("There was a problem obtaining this item: %s".formatted(id));
+            this.plugin.debug("ItemsAdder: %s".formatted(getIaStatus()));
+            this.plugin.debug("returning temporary AIR itemstack.");
+            return new ItemStack(Material.AIR);
+        }
+    }
+
+    @Contract(pure = true)
+    @Override
+    public @NotNull String getPrefix() {
+        return "itemsadder";
+    }
+
+    @Contract(pure = true)
+    @Override
+    public @NotNull String getPluginName() {
+        return "ItemsAdder";
+    }
+
+    @Contract(pure = true)
+    @Override
+    public @NotNull String getAuthor() {
+        return "Lapzupi Development Team";
+    }
+
+    @Contract(pure = true)
+    @Override
+    public @NotNull String getVersion() {
+        return "1.0.0";
+    }
+
+    @EventHandler
+    public void onItemsAdderLoad(ItemsAdderLoadDataEvent event) {
+        this.plugin.getLogger().info("Detected that ItemsAdder has loaded, reloading...");
+        this.iaStatus = true;
+        this.plugin.onReload();
+    }
+
+    @Contract(pure = true)
+    private @NotNull String getIaStatus() {
+        return iaStatus ? "Loaded" : "Not Loaded";
+    }
+}
